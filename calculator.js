409483e63a0472f66958540c7409ed62d8d7168d
Kavya Biederman <kavya.biederman@warriorlife.net>
2017-12-11 14:37:45 -0800

    Added function d, v, and e
    * Added function d which changes display
    * Added the function v which helps with the type numbers and operators
    * Added the function e which evaluates the equation

--- calculator.js
+++ calculator.js
@@ -1,1 +1,23 @@
 // JavaScript Document
+
+//Change Display
+function d(val) {
+	document.getElementById("d") . value = val;
+}
+
+//type numbers and operators 
+function v(val) {
+	document.getElementById("d"). value += val;
+	
+}
+
+// Evaluate the equation
+function e () {
+	try{
+		d(eval(document.getElementById("d").value));
+	}
+	catch(err) {
+		d("Error");	
+	}
+	
+}
